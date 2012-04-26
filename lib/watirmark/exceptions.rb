f34714c981d8df50e1db80fa116fbdb2ba7deb60
hmcgowan <hugh_mcgowan@yahoo.com>
2012-04-26 13:45:28 -0500

    add comments and remove unnecessary calls

--- lib/watirmark/exceptions.rb
+++ lib/watirmark/exceptions.rb
@@ -1,10 +1,6 @@
 module Watirmark
-
-  # This is probably what you should use
   class Watirmark::TestError < RuntimeError ;end
-  # Use this to abort your test
   class Watirmark::TestFailure < RuntimeError ;end
-  # Use this in error-checkers.
   class Watirmark::PostFailure < RuntimeError ;end
   class Watirmark::TDPage < RuntimeError; end
   class Watirmark::SecurityIssue < RuntimeError; end
