6d6264d63046172f6b6804ced171c453fcfce2b0
hmcgowan <hugh_mcgowan@yahoo.com>
2012-04-29 10:51:17 -0500

    Remove Ruby 1.8.7 code

--- lib/watirmark/extensions/ruby_extensions.rb
+++ lib/watirmark/extensions/ruby_extensions.rb
@@ -91,47 +91,22 @@ class Array
   end
 end
 
+
+#############################
+# From Why's Pognant Guide
+#############################
 class Object
-
-  if RUBY_VERSION =~ /^1\.8/
-    #############################
-    # This should be in Ruby 1.9
-    #############################
-    module InstanceExecHelper; end
-    include InstanceExecHelper
-
-    def instance_exec(*args, &block)
-      begin
-        old_critical, Thread.critical = Thread.critical, true
-        n = 0
-        n += 1 while respond_to?(mname="__instance_exec#{n}")
-        InstanceExecHelper.module_eval{ define_method(mname, &block) }
-      ensure
-        Thread.critical = old_critical
-      end
-      begin
-        ret = send(mname, *args)
-      ensure
-        InstanceExecHelper.module_eval{ remove_method(mname) } rescue nil
-      end
-      ret
-    end
-  end
-
-  #############################
-  # From Why's Pognant Guide
-  #############################
   def meta_class; class << self; self; end; end
   def meta_eval &blk; meta_class.instance_eval &blk; end
-  
+
   # Adds methods to a metaclass
   def meta_def name, &blk
     meta_eval { define_method name, &blk }
   end
-  
+
   # Defines an instance method within a class
   def class_def name, &blk
     class_eval { define_method name, &blk }
-  end  
+  end
 end
 
