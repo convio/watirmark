b014eadd4f8ca4dbae7095c6a987a5781582b428
Boris Jonica <bjonica@convio.com>
2012-05-29 15:31:14 -0500

    adding SmokeTestTask class for parallelized testing

---
+++ lib/watirmark/rake/smoketest.rb
@@ -0,0 +1,16 @@
+class SmokeTestTask < Rake::Task
+  def initialize(task_name=:smoke, tests)
+    raise "tests must be Hash" unless Hash === tests
+
+    @task_name = task_name
+
+    desc "Smoke Test"
+    task @task_name do
+      tests.each do |group_name, group_tasks|
+        multitask group_name => group_tasks
+        Rake::MultiTask[group_name].invoke
+      end
+    end
+  end
+end
+
