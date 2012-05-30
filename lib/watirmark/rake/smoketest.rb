3ee510ca501e16e7ca7934aa16c8b6e6f2945df4
Boris Jonica <bjonica@convio.com>
2012-05-30 14:43:54 -0500

    fixed syntax error in smoketest

--- lib/watirmark/rake/smoketest.rb
+++ lib/watirmark/rake/smoketest.rb
@@ -8,19 +8,19 @@ class SmokeTestTask
       # when using Hash, tasks may run in parallel. a new multitask
       # named group_name is created, which runs dependencies group_tasks
       # in parallel. Tasks in group_tasks must be defined beforehand
-      if tests.kind_of? Hash do
+      if tests.kind_of? Hash
         tests.each do |group_name, group_tasks|
           multitask group_name => group_tasks
           Rake::MultiTask[group_name].invoke
         end
+      elsif tests.kind_of? Array
       # when using an Array, tasks are run sequentially. These tasks
       # should already be defined beforehand
-      elsif tests.kind_of? Array do
         tests.each do |t|
           Rake::Task[t].invoke
         end
-      # currently, only Hash and Array task lists are supported
       else
+      # currently, only Hash and Array task lists are supported
         raise "tests must be either Hash or Array"
       end
     end
