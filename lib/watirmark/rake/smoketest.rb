ba7cf91ef3e6f7ef120e30fcb62b69d01acd498b
Boris Jonica <bjonica@convio.com>
2012-05-30 14:18:46 -0500

    name of task created by SmokeTestTask can no longer be overwritten

--- lib/watirmark/rake/smoketest.rb
+++ lib/watirmark/rake/smoketest.rb
@@ -1,21 +1,21 @@
 class SmokeTestTask
   include ::Rake::DSL if defined?(::Rake::DSL)
-  def initialize(task_name=:smoke, tests)
-    @task_name = task_name
+  def initialize(tests)
+    @task_name = :smoke
 
     desc "Smoke Test"
     task @task_name do
       # when using Hash, tasks may run in parallel. a new multitask
       # named group_name is created, which runs dependencies group_tasks
       # in parallel. Tasks in group_tasks must be defined beforehand
-      if Hash === tests do
+      if tests.kind_of? Hash do
         tests.each do |group_name, group_tasks|
           multitask group_name => group_tasks
           Rake::MultiTask[group_name].invoke
         end
       # when using an Array, tasks are run sequentially. These tasks
       # should already be defined beforehand
-      elsif Array === tests do
+      elsif tests.kind_of? Array do
         tests.each do |t|
           Rake::Task[t].invoke
         end
