b061cc1561bc0cf579662451c117e3d3ee0c4ea6
hmcgowan <hugh_mcgowan@yahoo.com>
2012-07-20 13:33:06 -0500

    refactor base class to use the new default class

--- lib/watirmark/models/simple.rb
+++ lib/watirmark/models/simple.rb
@@ -1,63 +1,116 @@
 module Watirmark
   module Model
     module Simple
+      attr_reader :defaults, :name, :uuid, :model_name
 
-      attr_accessor :__name__
-      attr_reader :uuid
-
-      def defaults
-        {}
+      def self.included(klass)
+        klass.extend Watirmark::Model::ClassMethods
       end
 
-      def initialize params = {}
-        @__name__ = params if String === params
-        if Watirmark::Configuration.instance.uuid
-          @uuid = Watirmark::Configuration.instance.uuid
-        else
-          @uuid = UUID.new.generate(:compact)
-        end
-        update defaults
-        update params unless String === params
-        self
+      def initialize(params={})
+        @defaults = self.class.defaults
+        @composed_fields = self.class.composed_fields
+        @models = self.class.models
+        @params = params
+        reload_settings
       end
 
-      def uuid
-        # this name hack will all go away in the next refactor
-        if __name__
-          "#{__name__}_#{@uuid}"[0..40]
-        else
-          "#{@uuid}"[0..40]
-        end
+      # this is a unique name that can be defined after the model is instantiated.
+      # if the value changes, all initial settings are reloaded. This allows us
+      # to set it during model creation in cucumber and make the fields take on
+      # a string that we used to identify the model in the gherkin
+      def model_name=(x)
+        @model_name = x
+        reload_settings
       end
 
-      # adds a hash of values to the object
-      def update hash
-        hash.each_pair do |key, value|
-          send "#{key}=", value
-        end
-        self
+      def models
+        model_list = [self]
+        @models.each{|x| model_list += x.models}
+        model_list.flatten.uniq
       end
 
-      # does the model include the hash's values
+      # This method is used to test the model to see if the updates to the model
+      # have already been applied. This allows the user to skip a step
+      # if something has already been applied. Useful for rerunning scripts, eg.
       def includes? hash
-        hash.each_pair do |key, value|
-          return false unless send("#{key}") == value
-        end
+        hash.each_pair { |key, value| return false unless send("#{key}") == value }
         true
       end
 
-      #adds a hash of values to the object but only if it's set up in the struct
-      def update_existing_members hash
-        hash.each_pair do |key, value|
-          send "#{key}=", value if respond_to? "#{key}=".to_sym
-        end
+
+      # Update the model with the provided hash
+      def update hash
+        hash.each_pair { |key, value| send "#{key}=", value }
         self
       end
+
+
+      # Update the model with the hash, only for members that exist in this model.
+      # TODO: this may be unncessary after we add composition of models
+      def update_existing_members hash
+        hash.each_pair { |key, value| send "#{key}=", value if respond_to? "#{key}=".to_sym }
+        self
+      end
+
 
       def to_h
         h = {}
         each_pair { |name, value| h[name] = value unless value.nil? }
         h
+      end
+
+
+      private
+
+      def reload_settings
+        set_defaults
+        define_composed_fields
+        generate_uuid
+        update @params
+        add_models
+      end
+
+      def set_defaults
+        @defaults.each do |name|
+          next unless respond_to? "#{name}="
+          val = @defaults.send(name)
+          if val.kind_of?(Proc)
+            send "#{name}=", instance_eval(&val)
+          else
+            send "#{name}=", val
+          end
+        end
+      end
+
+
+      # Generate a UUID for the instantiation of the model
+      # This id should be used in cases where the data needs to be
+      # unique for each test execution
+      def generate_uuid
+        if Watirmark::Configuration.instance.uuid
+          @uuid = @model_name.to_s + Watirmark::Configuration.instance.uuid
+        else
+          @uuid = @model_name.to_s + UUID.new.generate(:compact)
+        end
+      end
+
+
+      def define_composed_fields
+        @composed_fields.each_key do |method_name|
+          meta_def method_name do
+            instance_eval &@composed_fields[method_name]
+          end
+        end
+      end
+
+      def add_models
+        @models.each do |model|
+          method_name = model.class.to_s.sub(/Model$/, '').downcase
+          meta_def method_name do
+            model
+          end
+        end
       end
     end
   end
