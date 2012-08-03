e1d8ca882b2aa2469dbc2d6a9c43bff22c8baf50
hmcgowan <hugh_mcgowan@yahoo.com>
2012-08-03 13:50:00 -0500

    add support for models with namespaces

--- lib/watirmark/cucumber/transforms.rb
+++ lib/watirmark/cucumber/transforms.rb
@@ -1,14 +1,26 @@
+module Watirmark
+  module Transforms
+    def self.new_model model_name, user_defined_name
+      model_name = "#{model_name}Model".camelcase
+      DataModels.instance ||= {}
+      unless DataModels.instance.has_key?(user_defined_name)
+        model_class = TransformHelper.find_class_by_name(model_name.to_sym)
+        model = model_class.new
+        model.model_name = user_defined_name
+        DataModels.instance[user_defined_name] = model
+      end
+      DataModels.instance[user_defined_name]
+    end
+  end
+end
+
 # Create a new models and add it to the DataModels hash
 NEW_MODEL = Transform /^\[new ([^:]+): (\S+)\]$/ do |model_name, user_defined_name|
-  model_name = "#{model_name}Model".camelcase
-  DataModels.instance ||= {}
-  unless DataModels.instance.has_key?(user_defined_name)
-    model_class = TransformHelper.find_class_by_name(model_name.to_sym)
-    model = model_class.new
-    model.model_name = user_defined_name
-    DataModels.instance[user_defined_name] = model
-  end
-  DataModels.instance[user_defined_name]
+  Watirmark::Transforms.new_model model_name, user_defined_name
+end
+
+NEW_NAMESPACED_MODEL = Transform /^\[new (\w+::\w+) (\S+)\]$/ do |model_name, user_defined_name|
+  Watirmark::Transforms.new_model model_name, user_defined_name
 end
 
 # Return the models from the collection of existing models
