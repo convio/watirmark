7ad559bee2e8ee77ec3e2274c3297af78f78fdf6
hmcgowan <hugh_mcgowan@yahoo.com>
2012-08-22 10:22:50 -0500

    save screenshots after each cucumber step for smoke tests

--- lib/watirmark/cucumber/hooks.rb
+++ lib/watirmark/cucumber/hooks.rb
@@ -6,3 +6,14 @@ end
 Before('~@catch-post-failure') do
   Watirmark::Session.instance.post_failure = nil
 end
+
+if Watirmark::Configuration.instance.webdriver
+  FileUtils.rm_rf('reports')
+  FileUtils.mkdir('reports')
+  Page.browser.screenshot.base64
+  After do |scenario|
+    image = "reports/screenshot+#{UUID.new.generate(:compact)}.png"
+    Page.browser.screenshot.save image
+    embed File.expand_path(image), 'image/png'
+  end
+end
