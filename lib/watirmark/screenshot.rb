9618447eeb8fd9289640b9fd1b0c34d576b4b1cb
hmcgowan <hugh_mcgowan@yahoo.com>
2012-08-27 14:10:13 -0500

    add new screenshot module

---
+++ lib/watirmark/screenshot.rb
@@ -0,0 +1,39 @@
+require 'watirmark/configuration'
+Watirmark::Configuration.instance.defaults = {:create_master_snapshots => false}
+
+module Watirmark
+  module Screenshot
+
+    def self.take
+      Current.new
+    end
+
+    class Current
+      attr_accessor :filename
+
+      def initialize
+        @filename = "reports/screenshots/#{UUID.new.generate(:compact)}.png"
+        Page.browser.screenshot.save @filename
+      end
+
+      def md5
+        Digest::MD5.hexdigest(File.read(@filename))
+      end
+    end
+
+    class Master < Current
+      def initialize(filename, screenshot)
+        @filename = filename
+        update(screenshot) if Watirmark::Configuration.instance.create_master_snapshots
+      end
+
+      def update(screenshot)
+        File.unlink(@filename) if File.exists?(@filename)
+        FileUtils.copy_file(screenshot.filename, @filename)
+        puts "Created new master: #{@filename}"
+      end
+    end
+
+  end
+end
+
