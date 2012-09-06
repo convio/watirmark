332fa9bb2855e30addfd97841c4e660396b1476b
hmcgowan <hugh_mcgowan@yahoo.com>
2012-09-06 15:57:15 -0500

    fix models and namespaces so it works with nested namespaces too

--- lib/watirmark/cucumber/transforms.rb
+++ lib/watirmark/cucumber/transforms.rb
@@ -16,18 +16,15 @@ module Watirmark
 end
 
 # Create a new models and add it to the DataModels hash
-NEW_MODEL = Transform /^\[new ([^:]+): (\S+)\]$/ do |model_name, user_defined_name|
-  Watirmark::Transforms.new_model model_name, user_defined_name
-end
-
-NEW_NAMESPACED_MODEL = Transform /^\[new (\w+::\w+) (\S+)\]$/ do |model_name, user_defined_name|
+NEW_MODEL = Transform /^\[new (\S+) (\S+)\]$/ do |model_name, user_defined_name|
+  model_name.chop! if model_name.end_with?(':')
   Watirmark::Transforms.new_model model_name, user_defined_name
 end
 
 # Return the models from the collection of existing models
 MODEL = Transform /^\[(\S+)\]$/ do |model_name|
   DataModels.instance ||= {}
-  raise "#{model_name} is not a defined models!" unless DataModels.instance[model_name]
+  raise "#{model_name} is not a defined model!" unless DataModels.instance[model_name]
   DataModels.instance[model_name]
 end
 
