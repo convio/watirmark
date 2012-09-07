4ada5096801e06ead709461c2b277537fece144e
hmcgowan <hugh_mcgowan@yahoo.com>
2012-09-07 10:42:43 -0500

    add in transform for older way to use models.

--- lib/watirmark/cucumber/transforms.rb
+++ lib/watirmark/cucumber/transforms.rb
@@ -21,6 +21,12 @@ NEW_MODEL = Transform /^\[new (\S+) (\S+)\]$/ do |model_name, user_defined_name|
   Watirmark::Transforms.new_model model_name, user_defined_name
 end
 
+OLD_STYLE_MODEL = Transform /^\[new ([^:]+): (\S+)\]$/ do |model_name, user_defined_name|
+  model_name = model_name.camelcase
+  model_name.chop! if model_name.end_with?(':')
+  Watirmark::Transforms.new_model model_name, user_defined_name
+end
+
 # Return the models from the collection of existing models
 MODEL = Transform /^\[(\S+)\]$/ do |model_name|
   DataModels.instance ||= {}
