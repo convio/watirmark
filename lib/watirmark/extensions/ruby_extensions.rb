1580ae029fe86370154b4da785bf86bf62d89fe4
hmcgowan <hugh_mcgowan@yahoo.com>
2012-10-04 15:14:15 -0500

    remove unnecessary methods

--- lib/watirmark/extensions/ruby_extensions.rb
+++ lib/watirmark/extensions/ruby_extensions.rb
@@ -28,7 +28,7 @@ class FalseClass
   end
 end
 
-module HashExtension
+class Hash
   # Returns a hash that only contains pairs with the specified keys
   def extract(*keys)
     keys = keys[0] if keys[0].is_a?(Array)
@@ -36,49 +36,14 @@ module HashExtension
     keys.each { |k| hash[k] = self[k] if has_key?(k) }
     hash
   end
-
   alias :slice :extract
 end
 
-class Hash
-  include HashExtension
-end
-
 class String
-  class << self
-    def random(size=15, charset=:alnum)
-      charset =
-      case charset
-        when :alnum
-        [('0'..'9'),('A'..'Z'),('a'..'z')]
-        when :word
-        [('0'..'9'),('A'..'Z'),('a'..'z'),'_']
-        when :alpha
-        [('A'..'Z'),('a'..'z')]
-        when :digit
-        ['0'..'9']
-        when :ascii
-         (0..127).map{|c|c.chr}
-        when :asciiiso
-         (0..255).map{|c|c.chr}
-      else
-        charset
-      end.map{|it|it.to_a}.flatten
-         (0...size).inject(""){|s,c| s << charset[rand(charset.size)]}
-    end
-  end
-
   def strip_number_formatting
     self.gsub!(/\$|,/,'')
     self.gsub!(/\.00/,'')
     return self
-  end
-
-  # Some of the search widgets will abbreviate long
-  # strings so return an abbreviated string
-  def abbreviate(maxlength)
-    return self if length < maxlength
-    self[0..maxlength-2] + '...'
   end
 end
 
