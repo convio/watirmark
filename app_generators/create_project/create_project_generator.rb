9d1609e3eb59016a348e05c15980ccf16894dcdb
rubytester <github@rubytester.com>
2013-10-15 19:13:52 -0500

    resolve conflict with thor
    tweaking formatters

--- app_generators/create_project/create_project_generator.rb
+++ app_generators/create_project/create_project_generator.rb
@@ -1,4 +1,5 @@
 require 'thor/group'
+require 'active_support/inflector'
 
 class CreateProjectGenerator < Thor::Group
   include Thor::Actions
@@ -9,13 +10,13 @@ class CreateProjectGenerator < Thor::Group
     File.expand_path(File.join(File.dirname(__FILE__), 'templates'))
   end
 
-  def root
+  def root_files
     template "gemfile.rb.erb", "#{name}/Gemfile"
     template "config.yml.erb", "#{name}/config.yml"
     template "rakefile.rb.erb", "#{name}/rakefile.rb"
   end
 
-  def lib
+  def lib_files
     template "lib/configuration.rb.erb", File.join(name, "lib", name, "configuration.rb")
     template "lib/workflows.rb.erb", File.join(name, "lib", name, "workflows.rb")
     template "lib/core_libraries.rb.erb", File.join(name, "lib", name, "core_libraries.rb")
@@ -23,18 +24,18 @@ class CreateProjectGenerator < Thor::Group
     template "lib/name.rb.erb", File.join(name, "lib", "#{name}.rb")
   end
 
-  def lib_checkers
+  def lib_checkers_files
     template "lib/name/checkers/page_load_checker.rb.erb", File.join(name, "lib", name, "checkers", "page_load_checker.rb")
     template "lib/name/checkers/post_errors_checker.rb.erb", File.join(name, "lib", name, "checkers", "post_errors_checker.rb")
   end
 
-  def lib_site
+  def lib_site_files
     template "lib/name/site/base_controller.rb.erb", File.join(name, "lib", name, "site", "base_controller.rb")
     template "lib/name/site/search_controller.rb.erb", File.join(name, "lib", name, "site", "search_controller.rb")
     template "lib/name/site/base_view.rb.erb", File.join(name, "lib", name, "site", "base_view.rb")
   end
 
-  def features
+  def features_files
     template "features/model_steps.rb.erb", File.join(name, "features", "step_definitions", "model_steps.rb")
     template "features/post_error_steps.rb.erb", File.join(name, "features", "step_definitions", "post_error_steps.rb")
     template "features/site_steps.rb.erb", File.join(name, "features", "step_definitions", "site_steps.rb")
@@ -42,16 +43,17 @@ class CreateProjectGenerator < Thor::Group
     template "features/sample.feature.erb", File.join(name, "features", "#{name}_home.feature")
   end
 
-  def script
-    template "script/generate.rb.erb", File.join(name, "script", "generate.rb"), :chmod => 0755
+  def script_files
+    template "script/generate.rb.erb", File.join(name, "script", "generate.rb")
+    chmod File.join(name, "script", "generate.rb"), 0755
   end
 
-  def generators_mvc
-    template "generators/mvc/mvc_generator.rb.erb", File.join(name, "generators", "mvc", "mvc_generator.rb")
+  def generators_mvc_files
+    template "generators/mvc/generator.rb.erb", File.join(name, "generators", "mvc", "generator.rb")
     template "generators/mvc/rbeautify.rb.erb", File.join(name, "generators", "mvc", "rbeautify.rb")
-    template "generators/mvc/controller.rb.erb", File.join(name, "generators", "mvc", "templates", "controller.rb.erb")
-    template "generators/mvc/model.rb.erb", File.join(name, "generators", "mvc", "templates", "model.rb.erb")
-    template "generators/mvc/view.rb.erb", File.join(name, "generators", "mvc", "templates", "view.rb.erb")
-    template "generators/mvc/workflow_loader.rb.erb", File.join(name, "generators", "mvc", "templates", "workflow_loader.rb.erb")
+    template "generators/mvc/templates/controller.rb.erb", File.join(name, "generators", "mvc", "templates", "controller.rb.erb")
+    template "generators/mvc/templates/model.rb.erb", File.join(name, "generators", "mvc", "templates", "model.rb.erb")
+    template "generators/mvc/templates/view.rb.erb", File.join(name, "generators", "mvc", "templates", "view.rb.erb")
+    template "generators/mvc/templates/workflow_loader.rb.erb", File.join(name, "generators", "mvc", "templates", "workflow_loader.rb.erb")
   end
 end
