9699070a2daaecb0755da22abc1a336bbd7b7ca9
Donatello <donald.nguyen@blackbaud.com>
2013-10-23 17:58:01 -0500

    5.17.1 - Fix scenario when run doesn't exist

--- lib/watirmark/at_exit.rb
+++ lib/watirmark/at_exit.rb
@@ -2,7 +2,8 @@ module Watirmark
   def self.add_exit_task
     at_exit {
       if $!.nil? || $!.is_a?(SystemExit) && $!.success?
-        code = run(ARGV, $stderr, $stdout).to_i
+        code = 0
+        code = run(ARGV, $stderr, $stdout).to_i if respond_to?(:run)
       else
         code = $!.is_a?(SystemExit) ? $!.status : 1
       end
--- lib/watirmark/version.rb
+++ lib/watirmark/version.rb
@@ -1,5 +1,5 @@
 module Watirmark
   module Version
-    STRING = '5.17.0'
+    STRING = '5.17.1'
   end
 end
