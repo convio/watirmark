3bd32b038414dcf87712bd1d1957fdd6694f3395
kyle.martinez <kmartinez@convio.com>
2014-06-12 10:08:36 -0500

    shared examples for is deprecated

--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -1,16 +1,58 @@
 require_relative 'spec_helper'
 
-shared_examples_for "configuration_file" do
+describe "text file" do
+
+  before :all do
+    @config = Watirmark::Configuration.instance
+    @config.reset
+    @config.configfile = File.dirname(__FILE__) + '/configurations/config.txt'
+    @config.read_from_file
+  end
+
+ specify 'string' do
+   @config.string.should == "foo"
+ end
+
+ specify 'true_boolean' do
+   @config.true_boolean.should == true
+ end
+
+ specify 'false_boolean' do
+   @config.false_boolean.should == false
+ end
+
+ specify 'symbol' do
+   @config.symbol.should == :foo
+ end
+
+ specify 'integer' do
+   @config.integer.should == 3
+ end
+
+ specify 'float' do
+   @config.float.should == 1.2
+ end
+end
+
+describe "yaml file" do
+
+  before :all do
+    @config = Watirmark::Configuration.instance
+    @config.reset
+    @config.configfile = File.dirname(__FILE__) + '/configurations/config.yml'
+    @config.read_from_file
+  end
+
   specify 'string' do
     @config.string.should == "foo"
   end
 
   specify 'true_boolean' do
-    @config.true_boolean.should be_true
+    @config.true_boolean.should == true
   end
 
   specify 'false_boolean' do
-    @config.false_boolean.should be_false
+    @config.false_boolean.should == false
   end
 
   specify 'symbol' do
@@ -23,28 +65,6 @@ shared_examples_for "configuration_file" do
 
   specify 'float' do
     @config.float.should == 1.2
-  end
-end
-
-describe "text file" do
- it_behaves_like  "configuration_file"
-
-  before :all do
-    @config = Watirmark::Configuration.instance
-    @config.reset
-    @config.configfile = File.dirname(__FILE__) + '/configurations/config.txt'
-    @config.read_from_file
-  end
-end
-
-describe "yaml file" do
-  it_behaves_like  "configuration_file"
-
-  before :all do
-    @config = Watirmark::Configuration.instance
-    @config.reset
-    @config.configfile = File.dirname(__FILE__) + '/configurations/config.yml'
-    @config.read_from_file
   end
 end
 
