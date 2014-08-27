56887c7f5673becbdd7925af80ae2716b6f9f826
jogarcia@convio.com <Herp.Derp25>
2014-08-27 11:06:26 -0500

    added rspec tests for unique_instance_name

--- spec/model_factory_spec.rb
+++ spec/model_factory_spec.rb
@@ -5,7 +5,10 @@ describe "factory" do
   before :all do
     module FactoryTest
       class InitializeModel < Watirmark::Model::Factory
-        keywords :username, :password
+        keywords :username, :password, :test_name
+        defaults do
+          test_name { unique_instance_name }
+        end
       end
     end
   end
@@ -37,6 +40,16 @@ describe "factory" do
   specify 'to_h' do
     login = FactoryTest::InitializeModel.new(username: 'foo', password: 'bar')
     login.to_h.should == {:username=>"foo", :password=>"bar"}
+  end
+
+  specify "should generate custom unique_instance_name given model_name" do
+     demo_model = FactoryTest::InitializeModel.new({:model_name => "RspecUniqueName"})
+     demo_model.test_name.should =~ /^RspecUniqueName_[\dA-Za-z]+$/
+  end
+
+  specify "should generate default unique_instance_name given no model_name" do
+     demo_model = FactoryTest::InitializeModel.new()
+     demo_model.test_name.should =~ /^initialize_[\dA-Za-z]+$/
   end
 end
 
