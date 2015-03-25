c234de9c48c1d545f4102caf58d20734ab5c3b9f
Blackbaud-MatthewMazaika <matthew.mazaika@blackbaud.com>
2015-03-25 12:37:28 -0500

    adding tests to ensure we get a watir element when looking up siblings

--- lib/watirmark/extensions/webdriver_extensions.rb
+++ lib/watirmark/extensions/webdriver_extensions.rb
@@ -119,7 +119,7 @@ module Watir
 
     def previous_sibling
       e = locate_dom_element(:getPreviousSibling)
-      e.nil? ? element(xpath: './preceeding-sibling::*') : e
+      e.nil? ? element(xpath: './preceding-sibling::*') : e
     end
     alias_method :prev_sibling, :previous_sibling
     alias_method :prevsibling, :previous_sibling
