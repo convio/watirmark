2e7d4356a83ef34ae9aa63dcc63683d3777955e3
Titus Fortner <titusfortner@gmail.com>
2015-05-07 10:58:44 -0500

    Update for cucumber 2

--- lib/watirmark/cucumber/hooks.rb
+++ lib/watirmark/cucumber/hooks.rb
@@ -14,12 +14,10 @@ end
 
 After do |scenario|
   unless scenario.passed?
-    folder = scenario.file[/\/features\/([^\.]*)/, 1]
+    folder = scenario.location.to_s[/\/features\/([^\.]*)/, 1]
     feature = scenario.title.tr(' ', '_')
     (file, file_type) = HookHelper.take_screenshot(folder, feature)
     embed file, file_type
   end
   HookHelper.serialize_models
 end
-
-
