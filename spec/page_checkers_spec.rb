00ca0b88e3ddef0dd058661720409ace9a4a2312
Kyle Martinez <kyle.martinez@blackbaud.com>
2015-10-22 15:31:39 -0500

    need to start a new session to get checker to run

--- spec/page_checkers_spec.rb
+++ spec/page_checkers_spec.rb
@@ -6,9 +6,14 @@ describe "Watirmark post checkers" do
     @checker_ran = true
   end
 
+  def add_checker_to_session
+    Watirmark::Session::POST_WAIT_CHECKERS << Proc.new { post_checker_one }
+    Watirmark::Session.instance.newsession
+  end
+
   before :all do
     @checker_ran = false
-    Watirmark::Session::POST_WAIT_CHECKERS << Proc.new { post_checker_one }
+    add_checker_to_session
     @html = File.expand_path(File.dirname(__FILE__) + '/html/controller.html')
     Page.browser.goto "file://#{@html}"
   end
