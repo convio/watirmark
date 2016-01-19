78adb9b9309f088d1f9ce589d5a2f1a64c21f5ab
Matthew Mazaika <matthew.mazaika@blackbaud.com>
2016-01-19 09:22:59 -0600

    support chrome profile and bump version

--- lib/watirmark/session.rb
+++ lib/watirmark/session.rb
@@ -56,7 +56,18 @@ module Watirmark
         config.firefox_profile = default_firefox_profile
       elsif config.webdriver.to_s.eql? 'firefox_proxy'
         config.firefox_profile = proxy_firefox_profile(config.proxy_host, config.proxy_port)
+      elsif config.webdriver.to_s.eql? 'chrome'
+        config.chrome_profile = default_chrome_profile
       end
+    end
+
+    def default_chrome_profile
+      {
+          :download => {
+              :prompt_for_download => false,
+              :default_directory => download_directory
+          }
+      }
     end
 
     def default_firefox_profile
@@ -187,6 +198,8 @@ module Watirmark
         Watir::Browser.new :firefox, profile: config.firefox_profile, http_client: client
       when :sauce
         Watir::Browser.new use_sauce
+      when :chrome
+        Watir::Browser.new :chrome, prefs: config.chrome_profile, http_client: client
       else
         Watir::Browser.new config.webdriver.to_sym, http_client: client
       end
